dbe0e0f484709ef8a2961e631e348796112d3638
carlos <czanella@hotmail.com>
2016-06-20 19:45:59 -0300

    Speed up the plugin that cuts the assets

--- gimp plugins/cut_assets.py
+++ gimp plugins/cut_assets.py
@@ -7,6 +7,10 @@ import json
 from collections import defaultdict
 
 def extract_component(pixel_region, visited, px, py):
+    width = pixel_region.w
+    height = pixel_region.h
+    pixel_array = pixel_region[0:width,0:height]
+
     queue = Queue()
     queue.put((px, py))
 
@@ -23,22 +27,23 @@ def extract_component(pixel_region, visited, px, py):
             continue
 
         visited[x,y] = True
-        pixels[x,y] = pixel_region[x,y]
+        pixel_pos = 4*(x + y*width)
+        pixels[x,y] = pixel_array[pixel_pos:pixel_pos + 4]
         min_x = min(min_x, x)
         min_y = min(min_y, y)
         max_x = max(max_x, x)
         max_y = max(max_y, y)
 
-        if x > 0 and pixel_region[x-1,y][3] != '\x00' and not visited[x-1,y]:
+        if x > 0 and pixel_array[pixel_pos - 4 + 3] != '\x00' and not visited[x-1,y]:
             queue.put((x-1,y))
 
-        if x < pixel_region.w-1 and pixel_region[x+1,y][3] != '\x00' and not visited[x+1,y]:
+        if x < pixel_region.w-1 and pixel_array[pixel_pos + 4 + 3] != '\x00' and not visited[x+1,y]:
             queue.put((x+1,y))
 
-        if y > 0 and pixel_region[x,y-1][3] != '\x00' and not visited[x,y-1]:
+        if y > 0 and pixel_array[pixel_pos - 4*width + 3] != '\x00' and not visited[x,y-1]:
             queue.put((x,y-1))
 
-        if y < pixel_region.h-1 and pixel_region[x,y+1][3] != '\x00' and not visited[x,y+1]:
+        if y < pixel_region.h-1 and pixel_array[pixel_pos + 4*width + 3] != '\x00' and not visited[x,y+1]:
             queue.put((x,y+1))
 
     return {
@@ -51,6 +56,8 @@ def extract_component(pixel_region, visited, px, py):
 
 
 def cut_assets(img, drw, save_path, base_name):
+    from time import time
+    start_time = time()
     base_name = base_name or img.active_layer.name
     file_name_base = base_name + '_{}.png'
     full_path_base = os.path.join(save_path, file_name_base)
@@ -100,6 +107,7 @@ def cut_assets(img, drw, save_path, base_name):
     with open(json_full_path, 'w') as output_file:
         output_file.write(json.dumps(component_data))
 
+    pdb.gimp_message("Total time: {}".format(time() - start_time))
 
 register(
     'python_fu_cut_assets',
@@ -113,8 +121,8 @@ register(
     [
         (PF_IMAGE, 'image', 'Input image', None),
         (PF_DRAWABLE, 'drawable', 'Input drawable', None),
-        (PF_DIRNAME, 'save-path', 'Path for HTML export', os.getcwd()),
-        (PF_STRING, 'base-name', 'Base name for assets', ''),
+        (PF_DIRNAME, 'save-path', 'Path for file exports', os.getcwd()),
+        (PF_STRING, 'base-name', 'Base name for assets (leave blank to use layer name)', ''),
     ],
     [],
     cut_assets,
