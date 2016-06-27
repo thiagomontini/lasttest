2e6c0cd1f9c1ddc553b5f193bbce4d859624e654
carlos <czanella@hotmail.com>
2016-06-27 16:35:52 -0300

    Added a new server to better handle JS routes

---
+++ simpleServer.js
@@ -0,0 +1,49 @@
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs");
+
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
+
+var viewRoutes = [
+    "/",
+    "/form",
+    "/rio"
+];
+
+var port = 8000;
+
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename;
+
+    if (viewRoutes.indexOf(uri) >= 0) {
+        filename = path.join(process.cwd(), "public", "index.html");
+    }
+    else {
+        filename = path.join(process.cwd(), "public", uri);
+    }
+
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            console.log("not exists: " + filename);
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.write("404 Not Found\n");
+            res.end();
+            return;
+        }
+        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+        res.writeHead(200, mimeType);
+
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(res);
+    });
+}).listen(port, function() {
+    console.log("Start listening in http://localhost:" + port);
+});
