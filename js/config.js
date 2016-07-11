b24cd33fd4e24a54c825b5a54f3d64083f82135d
carlos <czanella@hotmail.com>
2016-07-11 02:39:58 -0300

    Additional debugs

--- js/config.js
+++ js/config.js
@@ -1,12 +1,14 @@
+var url_root = "/spintheglobe";
+
 module.exports = {
     // The root of the app, for the routing resolver
-    "root": "/spintheglobe",
+    "root": url_root,
 
     // The URL for the API that retrieves the flights information
     "apiUrl": "http://www.momondo.com/api/3.0/Flights",
 
     // The necessary assets for the globe
-    "globeMapUrl": "/img/globe/earthmap1k.jpg",
+    "globeMapUrl": url_root + "/img/globe/earthmap1k.jpg",
 
     // A threshold value to trigger the globe spinning. The smaller it is,
     // the easier it is to trigger the spin
