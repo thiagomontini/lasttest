4973d9cf054aaacc2deea144ddbda5485bfeeaf3
carlos <czanella@hotmail.com>
2016-07-10 23:53:04 -0300

    Added a base animation for the globe view

--- js/views/globe.jsx
+++ js/views/globe.jsx
@@ -2,8 +2,38 @@ var React = require("react");
 var THREE = require("three");
 
 var Globe = React.createClass({
+    componentDidMount: function() {
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        this.camera.position.z = 1000;
+
+        var geometry = new THREE.BoxGeometry( 200, 200, 200 );
+        var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.scene.add(this.mesh);
+
+        this.renderer = new THREE.WebGLRenderer({canvas: this.refs.canvas});
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+        this.animate();
+    },
+
+    animate: function() {
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+
+        this.renderer.render(this.scene, this.camera);
+        this.animationId = requestAnimationFrame(this.animate);
+    },
+
+    componentWillUnmount: function() {
+        cancelAnimationFrame(this.animationId);
+    },
+
     render: function() {
-        return <canvas className="globe" />;
+        return <canvas ref="canvas" className="globe" />;
     }
 });
 
