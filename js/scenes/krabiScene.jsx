1c94a47c44ab3501a4ae0a9b57e88827041132ee
carlos <czanella@hotmail.com>
2016-07-22 14:14:41 -0300

    Added clouds in Krabi

--- js/scenes/krabiScene.jsx
+++ js/scenes/krabiScene.jsx
@@ -6,7 +6,7 @@ var sceneData = require("./sceneData.js");
 var SceneMixin = require("./sceneMixin.jsx");
 var Cloud = require("../sceneObjects/cloud.js");
 
-var config = sceneData.ny.config;
+var config = sceneData.krabi.config;
 
 var KrabiScene = React.createClass({
     sceneKey: "krabi",
@@ -15,6 +15,18 @@ var KrabiScene = React.createClass({
 
     initScene: function() {
         this.disposables = [];
+
+        // Animates the clouds
+        this.disposables.push(new Cloud(this.objects.cloud1, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud2, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud3, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud4, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud5, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud6, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud7, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud8, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud9, config.cloud));
+        this.disposables.push(new Cloud(this.objects.cloud10, config.cloud));
     },
 
     disposeScene: function() {
--- js/scenes/krabiSceneData.js
+++ js/scenes/krabiSceneData.js
@@ -1,16 +1,86 @@
+var sceneWidth = 2304;
+
 module.exports = {
     "name": "Krabi",
     "lat": 98.4334108,
     "lon": 8.0404036,
-    "sceneWidth": 2304,
+    "sceneWidth": sceneWidth,
     "sceneHeight": 1440,
     "objects": [
         {
             "id": "background",
             "image": "img/krabi/Krabi_Back.jpg",
             "position": [0, 0]
+        },
+        {
+            "id": "cloud1",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_1.png"],
+            "position": [91, 62]
+        },
+        {
+            "id": "cloud2",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_2.png"],
+            "position": [453, 19]
+        },
+        {
+            "id": "cloud3",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_3.png"],
+            "position": [518, 59]
+        },
+        {
+            "id": "cloud4",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_4.png"],
+            "position": [1045,  9]
+        },
+        {
+            "id": "cloud5",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_5.png"],
+            "position": [1327, 70]
+        },
+        {
+            "id": "cloud6",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_6.png"],
+            "position": [1431, 72]
+        },
+        {
+            "id": "cloud7",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_7.png"],
+            "position": [1692, 25]
+        },
+        {
+            "id": "cloud8",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_8.png"],
+            "position": [1705, 64]
+        },
+        {
+            "id": "cloud9",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_9.png"],
+            "position": [1806, 19]
+        },
+        {
+            "id": "cloud10",
+            "image": "img/krabi/Clouds.json",
+            "frames": ["img/krabi/Clouds_10.png"],
+            "position": [2022, 67]
         }
     ],
     "hitAreas": [],
-    "config": {}
+    "config": {
+        "cloud": {
+            "speed": 70,
+            "yMin": 0,
+            "yMax": 250,
+            "sceneWidth": sceneWidth,
+            "speedVariance": 0.5
+        }
+    }
 };
